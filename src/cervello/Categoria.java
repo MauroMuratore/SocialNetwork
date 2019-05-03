bd0c4f8539ecf4b0e88ecf706f05f110e9402f68
MauroMuratore <m.muratore001@studenti.unibs.it>
2019-05-03 17:34:14 +0200

    aggiunti i metodi

--- src/cervello/Categoria.java
+++ src/cervello/Categoria.java
@@ -1,11 +1,30 @@
 package cervello;
 
 import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * IDEA POTREMMO METTERLO GENERICS, COSÌ GLI EVENTI SARANNO SOLO DELLA CATEGORIA GIUSTA 
+ */
 
 public abstract class Categoria {
 	
 	private String nome;
 	private String descrizione;
-	private ArrayList<? extends Evento> bacheca;
+	private ArrayList<Evento> bacheca;
+	
+	public Categoria(String _nome, String _descrizione) {
+		nome= _nome;
+		descrizione=_descrizione;
+		bacheca = new ArrayList();
+	}
+	
+	public void aggiungiEvento(Evento evento) {
+		bacheca.add(evento);
+	}
+	
+	public ArrayList<Evento> getBacheca(){
+		return bacheca;
+	}
 
 }
--- src/cervello/PartitaCalcioCat.java
+++ src/cervello/PartitaCalcioCat.java
@@ -2,4 +2,8 @@ package cervello;
 
 public class PartitaCalcioCat extends Categoria {
 
+	public PartitaCalcioCat(String _nome, String _descrizione) {
+		super(_nome, _descrizione);
+	}
+
 }
