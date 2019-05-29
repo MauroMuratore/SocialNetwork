6fb5e8fbd6db4742f65fb29db8ad41607fffbcae
MauroMuratore <m.muratore001@studenti.unibs.it>
2019-05-29 16:57:21 +0100

    aggiunto attributo partecipanti, metodi cambiaStato, iscrizione, valido, costruttore vuoto, setter

--- src/cervello/Evento.java
+++ src/cervello/Evento.java
@@ -2,32 +2,71 @@ package cervello;
 
 import java.sql.Time;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
+
+import database.NomiDB;
 
 //FINISHIM
 public abstract class Evento {
 	
 	private int idEvento;
-	private Campo<String> titolo;
-	private Campo<Integer> partecipantiMax;
-	private Campo<GregorianCalendar> termineUltimo;
-	private Campo<String> luogo;
-	private Campo<GregorianCalendar> dataInizio;
-	private Campo<Integer> durata;
-	private Campo<Integer> quotaIndividuale;
-	private Campo<String> compresoQuota;
-	private Campo<GregorianCalendar> dataFine;
-	private Campo<String> note;
+	private StatoEvento stato;
+	protected LinkedList<String> partecipanti;
+	protected Campo<String> titolo;
+	protected Campo<Integer> partecipantiMax;
+	protected Campo<GregorianCalendar> termineUltimo;
+	protected Campo<String> luogo;
+	protected Campo<GregorianCalendar> dataInizio;
+	protected Campo<Integer> durata;
+	protected Campo<Integer> quotaIndividuale;
+	protected Campo<String> compresoQuota;
+	protected Campo<GregorianCalendar> dataFine;
+	protected Campo<String> note;
 	
 	
-	
-	public Evento(int idEvento, Campo<String> titolo, Campo<Integer> partecipantiMax, 
+	/**
+	 * Da usare per la lettura da disco
+	 * 
+	 */
+	public Evento(int idEvento, Campo<String> titolo, Campo<Integer> partecipantiMax, LinkedList<String> partecipanti,
 			Campo<GregorianCalendar> termineUltimo, Campo<String> luogo, Campo<GregorianCalendar> dataInizio,
 			Campo<Integer> durata, Campo<Integer> quotaIndividuale, Campo<String> compresoQuota,
 			Campo<GregorianCalendar> dataFine, Campo<String> note) {
 		super();
 		this.idEvento = idEvento;
+		this.titolo = titolo;
+		this.partecipantiMax = partecipantiMax;
+		this.partecipanti=(LinkedList<String>) partecipanti.clone();
+		this.termineUltimo = termineUltimo;
+		this.luogo = luogo;
+		this.dataInizio = dataInizio;
+		this.durata = durata;
+		this.quotaIndividuale = quotaIndividuale;
+		this.compresoQuota = compresoQuota;
+		this.dataFine = dataFine;
+		this.note = note;
+	}
+	
+	/**
+	 * per creare un nuovo evento
+	 * @param titolo
+	 * @param partecipantiMax
+	 * @param termineUltimo
+	 * @param luogo
+	 * @param dataInizio
+	 * @param durata
+	 * @param quotaIndividuale
+	 * @param compresoQuota
+	 * @param dataFine
+	 * @param note
+	 */
+	public Evento(Campo<String> titolo, Campo<Integer> partecipantiMax,
+			Campo<GregorianCalendar> termineUltimo, Campo<String> luogo, Campo<GregorianCalendar> dataInizio,
+			Campo<Integer> durata, Campo<Integer> quotaIndividuale, Campo<String> compresoQuota,
+			Campo<GregorianCalendar> dataFine, Campo<String> note) {
+		super();
 		this.titolo = titolo;
 		this.partecipantiMax = partecipantiMax;
 		this.termineUltimo = termineUltimo;
@@ -38,7 +77,14 @@ public abstract class Evento {
 		this.compresoQuota = compresoQuota;
 		this.dataFine = dataFine;
 		this.note = note;
+		this.idEvento = (int) System.currentTimeMillis();
+		this.partecipanti = new LinkedList<String>();
 	}
+	
+	public Evento() {
+		this.idEvento = (int) System.currentTimeMillis();
+	}
+	
 
 	public int getIdEvento() {
 		return idEvento;
@@ -52,6 +98,9 @@ public abstract class Evento {
 	}
 	public Campo<GregorianCalendar> getTermineUltimo() {
 		return termineUltimo;
+	}
+	public LinkedList<String> getPartecipanti(){
+		return partecipanti;
 	}
 	public Campo<String> getLuogo() {
 		return luogo;
@@ -75,11 +124,105 @@ public abstract class Evento {
 	public Campo<String> getNote() {
 		return note;
 	}
+
+
+	public void setTitolo(String _titolo) {
+		titolo.setValore(_titolo);;
+	}
+
+	public void setPartecipantiMax(int _partecipantiMax) {
+		partecipantiMax.setValore(_partecipantiMax);;
+	}
+
+	public void setTermineUltimo(GregorianCalendar _termineUltimo) {
+		termineUltimo.setValore(_termineUltimo);;
+	}
+
+	public void setLuogo(String _luogo) {
+		luogo.setValore(_luogo);;
+	}
+
+	public void setDataInizio(GregorianCalendar _dataInizio) {
+		dataInizio.setValore(_dataInizio);;
+	}
+
+	public void setDurata(int _durata) {
+		durata.setValore(_durata);;
+	}
+
+	public void setQuotaIndividuale(int _quotaIndividuale) {
+		quotaIndividuale.setValore(_quotaIndividuale);;
+	}
+
+	public void setCompresoQuota(String _compresoQuota) {
+		compresoQuota.setValore(_compresoQuota);;
+	}
+
+	public void setDataFine(GregorianCalendar _dataFine) {
+		dataFine.setValore(_dataFine);;
+	}
+
+	public void setNote(String _note) {
+		note.setValore(_note);
+	}
 	
+	/**
+	 * torna vero se tutti i campi obbligatori sono stati compilati
+	 * @return
+	 */
+	public abstract boolean valido(); 
+		
+	
+
 	public String toString() {
 		return titolo.toString() + partecipantiMax.toString() + termineUltimo.toString() + luogo.toString() + dataInizio.toString() +durata.toString() + quotaIndividuale.toString() +
 				compresoQuota.toString() + dataFine.toString() + note.toString();
 	}
 	
+	/**
+	 * cambia lo stato dell'evento e torna una notifica 
+	 * @return null se lo stato non è cambiato
+	 */
+	public Notifica cambioStato() {
+		Date date = new Date(System.currentTimeMillis()); 
+		GregorianCalendar oggi = new GregorianCalendar();
+		oggi.setTime(date);
+		//passaggio da aperto a
+		if(stato==StatoEvento.APERTO) {
+			//chiuso
+			if(termineUltimo.getValore().after(oggi) || termineUltimo.getValore().equals(oggi)) {
+				if(partecipantiMax.getValore().intValue() == partecipanti.size()) {
+					stato=StatoEvento.CHIUSO;
+					return new Notifica(this, Notifica.CHIUSO);
+				}					
+			}
+			//fallito
+			if(termineUltimo.getValore().before(oggi)){
+				stato=StatoEvento.FALLITO;
+				return new Notifica(this, Notifica.FALLITO);
+			}
+		}
+		else if(stato==StatoEvento.CONCLUSO) {
+			if(dataFine.getValore().before(oggi)) {
+				stato=StatoEvento.CONCLUSO;
+				return new Notifica(this, Notifica.CONCLUSO);
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * iscrive un nuovo utente all'evento
+	 * @param nome
+	 * @return
+	 */
+	public Notifica iscrizione(String nome) {
+		if(stato==StatoEvento.APERTO) {
+			partecipanti.add(nome);
+			return new Notifica(this, Notifica.ISCRIZIONE);
+		}
+		return null;
+	}
 
 }
