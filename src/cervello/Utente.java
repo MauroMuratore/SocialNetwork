30629881db8db6d7ab8e2cf5c6746fdd2270679f
MauroMuratore <m.muratore001@studenti.unibs.it>
2019-06-12 17:15:49 +0100

    aggiunto controllo sull'utente

--- src/cervello/Utente.java
+++ src/cervello/Utente.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 //un utente � dafinito da username e pasword 
 public class Utente {
-	
+
 	private String username;
 	private byte[] password;
 	private LinkedList<Notifica> notifiche;
@@ -19,7 +19,7 @@ public class Utente {
 	public static final int RIMUOVI_INTERESSE = 30;
 	public static final int AGGIUNGI_INTERESSE = 40;
 	public static final String MODIFICA_RIUSCITA = "Modifica riuscita";
-	
+
 	public Utente(String id,byte[] pw, int _etamin, int _etamax)
 	{
 		username=id;
@@ -30,7 +30,7 @@ public class Utente {
 		etamin=_etamin;
 		etamax=_etamax;
 	}
-	
+
 	public Utente(String id, byte[] pw, LinkedList<Notifica> _notifiche, LinkedList<String> _interessiCategorie, ArrayList<Integer> _eventiCreati, int _etamin, int _etamax) {
 		username=id;
 		password=pw;
@@ -40,7 +40,7 @@ public class Utente {
 		etamin = _etamin;
 		etamax = _etamax;
 	}
-	
+
 	public byte[] getPassword() {
 		return password;
 	}
@@ -48,29 +48,29 @@ public class Utente {
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public LinkedList<Notifica> getNotifiche(){
 		return notifiche;
 	}
-	
+
 	public void setEtaMin(int min) {
 		etamin=min;
 	}
-	
+
 	public void setEtaMax(int max) {
 		etamax=max;
 	}
-	
-	
-	
+
+
+
 	public int getEtaMin() {
 		return etamin;
 	}
-	
+
 	public int getEtaMax() {
 		return etamax;
 	}
-	
+
 	public boolean riceviNotifica(Notifica not) {
 		return notifiche.add(not);
 	}
@@ -78,31 +78,32 @@ public class Utente {
 	public void cancellaNotifica(Notifica notifica) {
 		if(notifiche.remove(notifica))
 			System.out.println("rimossa notifica " + notifica.getMessaggio());;
-		
+
 	}
-	
+
 	public void aggiungiInteresse(String categoria) {
-		interessiCategorie.add(categoria);
+		if(!interessiCategorie.contains(categoria))
+			interessiCategorie.add(categoria);
 	}
-	
+
 	public void removeInteresse(String categoria) {
 		interessiCategorie.remove(categoria);
 	}
-	
+
 	public LinkedList<String> getInteressi(){
 		return interessiCategorie;
 	}
-	
+
 	public void creaEvento(int idEvento) {
 		eventiCreati.add(idEvento);
 	}
-	
+
 	public ArrayList<Integer> getIdEventi(){
 		return eventiCreati;
 	}
-	
-	
-	
-	
+
+
+
+
 
 }
